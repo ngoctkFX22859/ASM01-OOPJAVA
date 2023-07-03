7f973bc79f3bae8a1d2386332fa0d934a74f81f2
Cục Cưng <ngoctkFX22859@funix.edu.vn>
2023-07-03 23:32:37 +0700

    Asm2: Done1

---
+++ src/vn/funix/FX22859/java/asm02/Bank.java
@@ -0,0 +1,36 @@
+package vn.funix.FX22859.java.asm02;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class Bank {
+    private String ID;
+    private List<Customer> customers;
+
+    public Bank() {
+        customers = new ArrayList<Customer>();
+
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public List<Customer> getCustomers() {
+        return customers;
+    }
+
+    public void addCustomer(Customer newCustomer) {
+        customers.add(newCustomer);
+    }
+
+    public boolean isCustomerExisted(String customerId) {
+        for (Customer customer : customers) {
+            if (Objects.equals(customerId, customer.getCustomerID())) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ src/vn/funix/FX22859/java/asm02/Customer.java
@@ -0,0 +1,64 @@
+package vn.funix.FX22859.java.asm02;
+
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+
+public class Customer extends User {
+    private List<Account> accounts;
+
+    public Customer(String name, String customerID) {
+        super(name, customerID);
+        accounts = new ArrayList<Account>();
+    }
+
+    public List<Account> getAccount() {
+        return accounts;
+    }
+
+    public boolean isPremium() {
+        for (Account account : accounts) {
+            if (account.isPremium())
+                return true;
+        }
+        return false;
+    }
+
+    public void addAccount(Account newAccount) {
+        accounts.add(newAccount);
+    }
+
+    public boolean isAccountExisted(String accountNumber) {
+        for (Account account : accounts) {
+            if (Objects.equals(accountNumber, account.getAccountNumber())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    public String getBalance() {
+        double total = 0;
+        for (Account account : accounts) {
+            total += account.getBalance();
+        }
+        DecimalFormat decimalFormat = new DecimalFormat("#,##0đ");
+        return decimalFormat.format(total);
+    }
+
+
+    public void displayInformation() {
+        String isPre = isPremium() ? "Premium" : "Normal";
+        System.out.println(getCustomerID() + "  " + getName() + "  " + isPre + "  " + getBalance());
+
+        int index = 1;
+        for (Account account : accounts) {
+            System.out.println(index + "  " + account.toString());
+            index++;
+        }
+    }
+
+}
